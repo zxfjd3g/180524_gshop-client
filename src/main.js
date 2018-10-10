db4d3f116988a56ab4df2e40a742dea0ddf1acde
zxfjd3g <258147149@qq.com>
2018-10-10 09:43:11 +0800

    router

--- src/main.js
+++ src/main.js
@@ -1,13 +1,18 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-
-Vue.config.productionTip = false
+import App from './App.vue'
+import router from './router'
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  components: { App },
-  template: '<App/>'
+
+  /* components: { App },
+   template: '<App/>'
+   */
+  /*render: function (createElement) {  // 函数返回的标签会被渲染到el中
+    return createElement(App)   // '<App/>'
+  }*/
+  render: h => h(App),
+
+  router,
 })
---
+++ src/router/index.js
@@ -0,0 +1,38 @@
+/*
+路由器对象模块
+ */
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import MSite from '../pages/MSite/MSite.vue'
+import Search from '../pages/Search/Search.vue'
+import Order from '../pages/Order/Order.vue'
+import Profile from '../pages/Profile/Profile.vue'
+
+Vue.use(VueRouter)
+
+export default new VueRouter({
+  // 所有路由
+  routes: [
+    {
+      path: '/msite',
+      component: MSite
+    },
+    {
+      path: '/search',
+      component: Search
+    },
+    {
+      path: '/order',
+      component: Order
+    },
+    {
+      path: '/profile',
+      component: Profile
+    },
+
+    {
+      path: '/',
+      redirect: '/msite'
+    },
+  ]
+})
